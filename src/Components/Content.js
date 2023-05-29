6642f85793e9392eb54b313ea30c41266f087dc9
Mindru-Bogdan <77194184+Mindru-Bogdan@users.noreply.github.com>
2023-05-29 14:32:57 +0300

    Desktop Solution

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,9 +1,21 @@
+import { useState } from 'react';
+
 import Avatar from '../../src/avatar-michelle.jpg';
-import IconShare from '../../src/icon-share.svg';
+import IconShareOff from '../../src/icon-share.svg';
+import IconShareOn from '../../src/icon-share-on.svg';
+import IconFacebook from '../../src/icon-facebook.svg';
+import IconTwitter from '../../src/icon-twitter.svg';
+import IconPinterest from '../../src/icon-pinterest.svg';
 
 import classes from './Content.module.css';
 
 const Content = () => {
+  const [shareState, setShareState] = useState(false);
+
+  const changeShareState = () => {
+    setShareState(!shareState);
+  };
+
   return (
     <div className={classes.card}>
       <h1>Shift the overall look and feel by adding these wonderful touches to furniture in your home</h1>
@@ -16,10 +28,17 @@ const Content = () => {
         <h2>Michelle Appleton</h2>
         <h3>28 Jun 2020</h3>
       </div>
-      <div className={classes.btn}>
-        <img src={IconShare} className={classes.iconShare} />
+      <div onClick={changeShareState} className={shareState ? classes.btnActive : classes.btnInactive}>
+        <img src={IconShareOn} onClick={changeShareState} className={shareState ? classes.iconShare : classes.hidden} />
+        <img src={IconShareOff} onClick={changeShareState} className={shareState ? classes.hidden : classes.iconShare} />
       </div>
-      {/* <div className={classes.share}>Share;</div> */}
+      <div className={shareState ? classes.shareCard : classes.hidden}>
+        <h4>SHARE</h4>
+        <img src={IconFacebook} className={classes.logo} />
+        <img src={IconTwitter} className={classes.logo} />
+        <img src={IconPinterest} className={classes.logo} />
+        <div className={shareState ? classes.shareCardEnd : classes.hidden}></div>
+      </div>
     </div>
   );
 };
